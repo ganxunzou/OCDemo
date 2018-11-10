d77c04459728a2f9b6c09c708e81f929ec0a92d7
ganxz <ganxz.sdc@sdc.com>
2018-11-10 17:04:43 +0800

    Initial Commit

---
+++ OCDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OCDemo
+//
+//  Created by GXZ on 18/11/10.
+//  Copyright © 2018年 GXZ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
